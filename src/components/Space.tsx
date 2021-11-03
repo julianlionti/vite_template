1dd061011bdd5b675b26a57fb3991abfc3662abd
Julian Lionti <julliont@publicisgroupe.net>
2021-11-03 16:49:00 -0300

    husky

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import React from 'react'
 
+
 type Props = Partial<{ isVert: boolean }>
 export const Space = styled.div<Props>`
   margin: ${({ isVert }) => `${isVert ? 10 : 0}px ${isVert ? 0 : 10}px`};
